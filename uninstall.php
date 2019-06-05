7391f3ee7b3361ea24a49e672cb51d109de2c7c3
Brandon Hubbard <bkhubbard@gmail.com>
2019-06-05 09:50:51 -0700

    lots of cleanup

---
+++ uninstall.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Uninstall.
+ *
+ * @package image-flip-for-woocommerce
+ */
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+
+if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
+	exit();
+}
+
+// Nothing to do for now. Have a Nice Day!
