225f736d8f124b86a96e385a8e99a4f5c98a6ab6
Brandon Hubbard <bkhubbard@gmail.com>
2019-06-05 10:00:51 -0700

    scaffold plugin

---
+++ image-flip-for-woocommerce.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Image Flip For Woocommerce
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     image-flip-for-woocommerce
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Image_Flip_For_Woocommerce
+ */
+
+// Your code starts here.
